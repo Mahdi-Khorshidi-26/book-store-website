6a76e93ec571140226a2898d6261f42aa8c3fff2
Mahdi.Khorshidi.26 <khorshidi.mahdi.1998@gmail.com>
2023-12-29 12:14:28 +0330

    animation controll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-//handling movements
+//handling movements with animations
+let pages = document.querySelector(".pages");
+let subMenu = document.querySelector(".sub-menu");
+
+pages.addEventListener("mouseover", function () {
+  subMenu.classList.add("submenu-in");
+  subMenu.classList.remove("submenu-out");
+});
+pages.addEventListener("mouseleave", function () {
+  subMenu.classList.add("submenu-out");
+  subMenu.classList.remove("submenu-in");
+});
